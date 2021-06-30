22d9b2523ce776d900e87dbe0308e13ddedecfa0
Vlad Ulitin <ylitin45@gmail.com>
2021-06-30 17:08:07 +0300

    Update Controller.java

--- Homework_2/Play_Java/src/com/company/Controller.java
+++ Homework_2/Play_Java/src/com/company/Controller.java
@@ -5,39 +5,54 @@ import java.util.Scanner;
 public class Controller {
     private View view;
     private Model model;
-    private Scanner scanner;
 
-    public Controller(View view,Model model){
-        this.view=view;
-        this.model=model;
-        scanner=new Scanner(System.in);
+
+    public Controller(View view, Model model) {
+        this.view = view;
+        this.model = model;
+
     }
-    public void start(){
-        boolean prov = false;
-        int userNumber;
-        do {
-            try {
-                userNumber = scanNumber(scanner);
-                prov = model.checkUserNumber(userNumber);
-            } catch (NumberFormatException e) {
-                view.show(view.TYPE_ERROR);
-            } catch (NumberGoingOfBoundsException e) {
-                view.show(view.RANGE_ERROR, model.from, model.to);
+
+    public void start() {
+        Scanner scanner = new Scanner(System.in);
+        model.setLine(GlobalConstants.PRIMARY_MIN_BARRIER, GlobalConstants.PRIMARY_MAX_BARRIER);
+
+
+        while (model.checkNumber(inputIntValueWithScanner(scanner)));
+
+        view.printMessage(View.CONGRATULATION );
+        view.printMessage(View.YOUR_WAY + String.valueOf(model.getAllNumber()));
+
+    }
+
+    private int inputIntValueWithScanner(Scanner sc) {
+        int res = 0;
+        view.printMessage(getInputIntMessage());
+        while (true) {
+
+            while (!sc.hasNextInt()) {
+                view.printMessage(View.WRONG_INPUT_INT_DATA + getInputIntMessage());
+
+                sc.next();
             }
-        } while (!prov);
-        view.show(view.LAST_MESSAGE);
-        view.show(view.LIST_MESSAGE, model.allNumbers);
 
+            if ((res = sc.nextInt()) <= model.getBottonLine() ||
+                    res >= model.getUpperLine()) {
+                view.printMessage(View.WRONG_INPUT_INT_DATA + getInputIntMessage());
+                continue;
+            }
+            break;
+        }
+
+        return res;
     }
-    private int scanNumber(Scanner scanner) {
-        view.show(view.LIST_MESSAGE, model.allNumbers);
-        view.show(view.RANGE_MESSAGE, model.from, model.to);
-        String userNumberStr = scanner.nextLine();
-        return Integer.parseInt(userNumberStr);
+
+    private String getInputIntMessage() {
+        return view.concatenationString(
+                View.INPUT_INT_DATA, View.OPENS_SQUARE_BRACKET,
+                String.valueOf(model.getBottonLine()), View.SPACE_SING,
+                String.valueOf(model.getUpperLine()),
+                View.CLOSING_SQUARE_BRACKET, View.SPACE_SING,
+                View.EQUAL_SING, View.SPACE_SING );
     }
-
-
-}
-class  NumberGoingOfBoundsException extends Exception{
-
 }
