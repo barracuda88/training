e61240fe120fac6044e60e3555e65832b09c76e7
Vlad Ulitin <ylitin45@gmail.com>
2021-07-05 13:23:20 +0300

    rename temlat on template

--- Homowork_4/src/templates/Template.java
+++ Homowork_4/src/templates/Template.java
@@ -1,6 +1,6 @@
 package templates;
 
-public class Templat {
+public class Template {
     public static final String namePattern="[A-Z][a-z]{2,}";
     public static final String surnamePattern="[A-Z][a-z]{2,}";
     public static final String nicknamePattern="^[A-Za-z0-9_-]{3,16}$";
