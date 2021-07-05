10b986ee01c5c54fdb2f5d6eab10d16412c8b49a
Vlad Ulitin <ylitin45@gmail.com>
2021-07-05 16:16:43 +0300

    Update Template

--- Homowork_4/src/templates/Template.java
+++ Homowork_4/src/templates/Template.java
@@ -4,4 +4,13 @@ public class Template {
     public static final String namePattern="[A-Z][a-z]{2,}";
     public static final String surnamePattern="[A-Z][a-z]{2,}";
     public static final String nicknamePattern="^[A-Za-z0-9_-]{3,16}$";
+    public static final String middleNamePattern="[A-Z][a-z]{2,}";
+    public static final String logginSkypePattern="^[A-Za-z0-9_-.@]{3,14}$";
+    public static final String emailPattern="[A-Za-z0-9-_]+(@gmail.com|@ukr.net)";
+    public static final String сityPattern="[A-Z][a-z]{2,}";
+    public static final String streetPattern="[A-Z][a-z]{2,}[-]*[A-Z]*[a-z]*";
+    public static final String indexPattern="[0-9]{3,}";
+
+
+
 }
