c55372ae5ef9f05c8c30835cb9ecddf5682efec0
Vlad Ulitin <ylitin45@gmail.com>
2021-07-07 17:23:26 +0300

    Create TextConstant.java

---
+++ Homowork_4/src/com/company/view/TextConstant.java
@@ -0,0 +1,12 @@
+package com.company.view;
+
+/**
+ * Created by Vlad Ulitin on 07.07.2021.
+ */
+public interface TextConstant {
+    String INPUT_INFO_DATA = "input.string.data";
+    String INPUT_LAST_NAME="input.last.name.date";
+    String INPUT_FIRST_NAME = "input.first.name.data";
+    String WRONG_INPUT_DATA = "input.wrong.data";
+    String INPUT_LOGIN_DATA = "input.login.data";
+}
