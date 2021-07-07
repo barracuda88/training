a8d5515ed4ebafb821c0a092537dbd5ca40a96b3
Vlad Ulitin <ylitin45@gmail.com>
2021-07-07 17:23:29 +0300

    Create Model.java

---
+++ Homowork_4/src/com/company/model/Model.java
@@ -0,0 +1,34 @@
+package com.company.model;
+
+/**
+ * Created by Vlad Ulitin on 07.07.2021.
+ */
+public class Model {
+    private String name;
+    private String surname;
+    private String login;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+}
